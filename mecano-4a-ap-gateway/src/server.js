4816d1035a83b8ded88b91c200eb1d6b05449cf4
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-11-29 15:15:50 -0500

    Typos urls backendsn correcciones

--- mecano-4a-ap-gateway/src/server.js
+++ mecano-4a-ap-gateway/src/server.js
@@ -1,4 +1,4 @@
 module.exports = {
     mecano_4a_be_usuarios_main: 'https://nebulosa-qwerty-usuarios.herokuapp.com/',
-    mecano_4a_be_niveles_dev: 'https://nebulosa-be-niveles.herokuapp.com/.herokuapp.com',
+    mecano_4a_be_niveles_dev: 'https://nebulosa-be-niveles.herokuapp.com/',
     };
