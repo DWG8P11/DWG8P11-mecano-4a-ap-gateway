52694e4f3f293c7a5bc3d879dc58adab2b23570d
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-11-30 10:31:29 -0500

    Typedefs Puntaje terminados

--- mecano-4a-ap-gateway/src/typeDefs/puntajes_type_defs.js
+++ mecano-4a-ap-gateway/src/typeDefs/puntajes_type_defs.js
@@ -3,32 +3,32 @@ const { gql } = require('apollo-server');
 // TODO Fecha...
 const puntajesTypeDefs = gql(`
     type PuntajeIn {
-        usuario: String!
-        precision: Float!
-        cpme: Int!
-        segundos: Int!
-        fecha: String
+        usuario     : String!
+        precision   : Float!
+        cpme        : Int!
+        segundos    : Int!
+        fecha       : String
     }
 
     type PuntajeOut {
-        id: String!
-        usuario: String!
-        precision: Float!
-        cpme: Int!
-        segundos: Int!
-        fecha: String
+        id          : String!
+        usuario     : String!
+        precision   : Float!
+        cpme        : Int!
+        segundos    : Int!
+        fecha       : String
     }
 
     extend type Query {
-        traerPuntajes(usuario: String, idLeccion: String): [PuntajeOut!]!
-        traerPuntajesUsuarioLeccion(nivel: Int!, nLeccion: Int!, usuario: String): [PuntajeOut!]!
-        traerPuntaje(idPuntaje: String!): PuntajeOut!
+        traerPuntajes               (usuario    : String, idLeccion : String                ): [PuntajeOut!]!
+        traerPuntajesUsuarioLeccion (nivel      : Int!  , nLeccion  : Int!, usuario: String ): [PuntajeOut!]!
+        traerPuntaje                (idPuntaje  : String!                                   ): PuntajeOut!
     }
 
     extend type Mutation {
-        crearPuntaje(puntaje: PuntajeIn!): PuntajeOut!
-        eliminarPuntaje(idPuntaje: String!): Int
-        eliminarPuntajes(usuario: String, idLeccion: String): Int
+        crearPuntaje    (puntaje    : PuntajeIn!                ): PuntajeOut!
+        eliminarPuntaje (idPuntaje  : String!                   ): Int
+        eliminarPuntajes(usuario    : String, idLeccion: String ): Int
     }
 `);
 
