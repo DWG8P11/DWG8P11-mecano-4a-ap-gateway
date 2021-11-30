f79812095d2aad95d9fc51ef93416bbb64e2c471
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-11-30 08:54:42 -0500

    Mejora pequeña y typo niveles typedefs

--- mecano-4a-ap-gateway/src/typeDefs/niveles_type_defs.js
+++ mecano-4a-ap-gateway/src/typeDefs/niveles_type_defs.js
@@ -4,21 +4,21 @@ const { gql } = require('apollo-server');
 // TODO mejorar eliminar en el back para que retorne el ID
 const nivelesTypeDefs = gql(`
     type Nivel {
-        id: Int!
-        nombre: String!
-        descripcion: String
-        imagen: String
+        id          : Int!
+        nombre      : String!
+        descripcion : String
+        imagen      : String
     }
 
     extend type Query {
-        traerNiveles: [Nivel!]!
-        traerNivel(idNivel: Int!): Nivel!
+        traerNiveles                : [Nivel!]!
+        traerNivel  (idNivel: Int!) : Nivel!
     }
 
     extend type Mutation {
-        crearNivel(nivel: Nivel!): Nivel!
-        actualizarNivel(idNivelViejo: Int!, nivelNuevo Nivel!): Nivel!
-        eliminarNivel(idNivel: Int!): Int
+        crearNivel      (nivel          : Nivel!                    )   : Nivel!
+        actualizarNivel (idNivelViejo   : Int!, nivelNuevo: Nivel!  )   : Nivel!
+        eliminarNivel   (idNivel        : Int!                      )   : Int
     }
 `);
 
