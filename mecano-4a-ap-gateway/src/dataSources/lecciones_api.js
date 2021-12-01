26df5435bcd1c6bf8042dec3a5eb46b855b1ec50
Andrea Quebrada <danseurwaslala@gmail.com>
2021-12-01 17:24:19 -0500

    Datasources lecciones y arreglo datasources niveles

---
+++ mecano-4a-ap-gateway/src/dataSources/lecciones_api.js
@@ -0,0 +1,40 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+const serverConfig       = require('../server');
+class LeccionesAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = serverConfig.mecano_4a_be_niveles_url;
+    }
+
+    async registrarLeccion(nuevaLeccion) {
+        nuevaLeccion = new Object(JSON.parse(JSON.stringify(nuevaLeccion)));
+        return await this.post(`/aprende/lecciones`,nuevaLeccion);
+    }
+
+    async traerLeccion(nivel,nLeccion) {
+        return await this.get(`/aprende/lecciones/${nivel}/${nLeccion}`);
+    }
+
+    async traerLeccionPorId(idLeccion) {
+        return await this.get(`/aprende/lecciones/${idLeccion}`);
+    }
+
+    async traertodasLecciones(nivel) {
+        return await this.get(`/aprende/lecciones/`,{nivel=nro_nivel});
+    }
+
+    async actualizarLeccion(nivelViejo, nLeccionViejo,leccionNueva) {
+        leccionNueva = new Object(JSON.parse(JSON.stringify(leccionNueva)));
+        return await this.put(`/aprende/lecciones/${nivelViejo}/${nLeccionViejo}`,leccionNueva);
+    }
+
+    async borrarLeccion_sin_detalle(nivel,nLeccion) {
+        return await this.delete(`/aprende/lecciones/${nivel}/${nLeccion}`);
+    }
+
+    async borrarLeccion(idLeccion) {
+        return await this.delete(`/aprende/lecciones/${idLeccion}`);
+    }
+   
+}
+module.exports = LeccionesAPI;
--- mecano-4a-ap-gateway/src/dataSources/niveles_api.js
+++ mecano-4a-ap-gateway/src/dataSources/niveles_api.js
@@ -6,36 +6,27 @@ class NivelesAPI extends RESTDataSource {
         this.baseURL = serverConfig.mecano_4a_be_niveles_url;
     }
 
-    async registrarLeccion(nuevaLeccion) {
-        nuevaLeccion = new Object(JSON.parse(JSON.stringify(nuevaLeccion)));
-        return await this.post(`/aprende/lecciones`,nuevaLeccion);
+    async registroNuevoNivel(nuevoNivel) {
+        nuevoNivel = new Object(JSON.parse(JSON.stringify(nuevoNivel)));
+        return await this.post(`/aprende/niveles`,nuevoNivel);
     }
 
-    async traerLeccion(nivel,nLeccion) {
-        return await this.get(`/aprende/lecciones/${nivel}/${nLeccion}`);
+    async traerNiveles() {
+        return await this.get(`/aprende/niveles`);
     }
 
-    async traerLeccionPorId(idLeccion) {
-        return await this.get(`/aprende/lecciones/${idLeccion}`);
+    async traerNivel(id) {
+        return await this.get(`/aprende/niveles/${id}`);
     }
 
-    async traertodasLecciones(nivel) {
-        return await this.get(`/aprende/lecciones/`,{nivel=nro_nivel});
+    async actualizarNivel(idViejo,nivelNuevo) {
+        nivelNuevo = new Object(JSON.parse(JSON.stringify(nivelNuevo)));
+        return await this.put(`/aprende/niveles/${idViejo}`, nivelNuevo);
     }
 
-    async actualizarLeccion(nivelViejo, nLeccionViejo,leccionNueva) {
-        leccionNueva = new Object(JSON.parse(JSON.stringify(leccionNueva)));
-        return await this.put(`/aprende/lecciones/${nivelViejo}/${nLeccionViejo}`,leccionNueva);
+    async borrarNivel(id) {
+        return await this.delete(`/aprende/niveles/${id}`);
     }
-
-    async borrarLeccion_sin_detalle(nivel,nLeccion) {
-        return await this.delete(`/aprende/lecciones/${nivel}/${nLeccion}`);
-    }
-
-    async borrarLeccion(idLeccion) {
-        return await this.delete(`/aprende/lecciones/${idLeccion}`);
-    }
-   
    
 }
 module.exports = NivelesAPI;
