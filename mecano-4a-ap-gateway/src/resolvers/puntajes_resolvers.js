05df445b1e25fba05865f8d0cb5251c899ca4b6d
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-12-03 06:07:44 -0500

    Esqueleto Puntajes Resolvers

--- mecano-4a-ap-gateway/src/resolvers/puntajes_resolvers.js
+++ mecano-4a-ap-gateway/src/resolvers/puntajes_resolvers.js
@@ -1,5 +1,31 @@
 const puntajesResolvers = {
+    Query: {
+        traerPuntajes: async function(_, {usuario, idLeccion}, context) {
 
+        },
+
+        traerPuntajesUsuarioLeccion: async function(_, {nivel, nLeccion, usuario}, context) {
+
+        },
+
+        traerPuntaje: async function(_, {idPuntaje}, context) {
+
+        },
+    },
+
+    Mutation: {
+        crearPuntaje: async function(_, {puntaje}, context) {
+
+        },
+
+        eliminarPuntaje: async function(_, idPuntaje, context) {
+
+        },
+
+        eliminarPuntajes: async function(_, {usuario, idLeccion}, context) {
+
+        },
+    }
 };
 
 module.exports = puntajesResolvers;
