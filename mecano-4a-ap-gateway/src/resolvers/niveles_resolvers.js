f831f583d39f617f673e8980888d7432649a6b00
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-12-03 05:49:20 -0500

    Esqueleto Niveles Resolvers y minimejora typedefs

--- mecano-4a-ap-gateway/src/resolvers/niveles_resolvers.js
+++ mecano-4a-ap-gateway/src/resolvers/niveles_resolvers.js
@@ -1,5 +1,27 @@
 const nivelesResolvers = {
+    Query: {
+        traerNivel: async function(_, {idNivel}, context) {
 
+        },
+
+        traerNiveles: async function(_, __, context) {
+
+        },
+    },
+
+    Mutation: {
+        registrarNivel: async function(_, {nivel}, context) {
+
+        },
+
+        actualizarNiveles: async function(_, {idNivelViejo, actualizacionInput}, context) {
+
+        },
+
+        eliminarNivels: async function(_, {idNivel}, context) {
+
+        },
+    }
 };
 
 module.exports = nivelesResolvers;
