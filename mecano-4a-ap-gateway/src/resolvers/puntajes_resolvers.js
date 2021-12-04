5839bc73af2c9e7eaa6173f00689d0a06e22650b
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-12-04 18:29:43 -0500

    Agregados Resolvers Puntajes. Falta eliminar por numero de leccion y traer puntajes de usuario en lecciones de nivel dado

--- mecano-4a-ap-gateway/src/resolvers/puntajes_resolvers.js
+++ mecano-4a-ap-gateway/src/resolvers/puntajes_resolvers.js
@@ -1,29 +1,45 @@
 const puntajesResolvers = {
     Query: {
-        traerPuntajes: async function(_, {usuario, idLeccion}, context) {
-
+        traerPuntajes: async function(_, {usuario, idLeccion}, contexto) {
+            if (usuario) {
+                if (!contexto.usuarioT || !contexto.usuarioT.es_administrador) {
+                    throw new ApolloError("No tienes los permisos para ver los puntajes de este usuario.", 401)
+                }
+            }
+            return contexto.dataSources.puntajesAPI.traerPuntajes(usuario, idLeccion);
         },
 
-        traerPuntajesUsuarioLeccion: async function(_, {nivel, nLeccion, usuario}, context) {
-
+        traerPuntajesUsuarioLeccion: async function(_, {nivel, nLeccion, usuario}, contexto) {
+            // TODO
         },
 
-        traerPuntaje: async function(_, {idPuntaje}, context) {
-
+        traerPuntaje: async function(_, {idPuntaje}, contexto) {
+            return contexto.dataSources.puntajesAPI.traerPuntaje(idPuntaje);
         },
     },
 
     Mutation: {
-        crearPuntaje: async function(_, {puntaje}, context) {
-
+        crearPuntaje: async function(_, {puntaje}, contexto) {
+            if (!contexto.usuarioT || !contexto.usuarioT.es_administrador) {
+                throw new ApolloError("No tienes los permisos para crear un nuevo puntaje.", 401)
+            }
+            return contexto.dataSources.puntajeesAPI.registrarPuntaje(puntaje);
         },
 
-        eliminarPuntaje: async function(_, idPuntaje, context) {
+        eliminarPuntaje: async function(_, idPuntaje, contexto) {
+            if (!contexto.usuarioT || !contexto.usuarioT.es_administrador) {
+                throw new ApolloError("No tienes los permisos para borrar un puntaje.", 401)
+            }
 
+            return  contexto.dataSources.puntajeesAPI.borrarPuntaje(idPuntaje);
         },
 
-        eliminarPuntajes: async function(_, {usuario, idLeccion}, context) {
+        eliminarPuntajes: async function(_, {usuario, idLeccion}, contexto) {
+            if (!contexto.usuarioT || !contexto.usuarioT.es_administrador) {
+                throw new ApolloError("No tienes los permisos para borrar un puntaje.", 401)
+            }
 
+            // TODO
         },
     }
 };
